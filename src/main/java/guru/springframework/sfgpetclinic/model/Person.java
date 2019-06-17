ceb4c39aa64545da47a4368bee7155b113c4c956
Hammed Adetola Adekola <godguide1@yahoo.com>
2019-06-17 22:34:15 +1000

    Adding POJOs. Closes #2.

--- src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -8,13 +8,6 @@ public class Person {
     private String firstName;
     private String lastName;
 
-    public Person() {}
-
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
     public String getFirstName() {
         return firstName;
     }
