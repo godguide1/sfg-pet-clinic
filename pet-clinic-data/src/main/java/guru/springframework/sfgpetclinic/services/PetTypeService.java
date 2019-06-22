e062f745348211273a2fcad171dc3626e32dc7af
Hammed Adetola Adekola <godguide1@yahoo.com>
2019-06-23 00:12:49 +1000

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Adetola on 22/06/2019
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
