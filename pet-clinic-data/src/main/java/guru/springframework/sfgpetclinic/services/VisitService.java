fdd8f6f9e9a78402e8c78271d8af44c2f6770f25
Hammed Adetola Adekola <godguide1@yahoo.com>
2019-06-24 04:41:50 +1000

    Implemented Map Impl for Visit Service. closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by Adetola on 24/06/2019
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
